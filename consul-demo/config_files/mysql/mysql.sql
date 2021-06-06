a76d45a00a8142f21546cc90c93ff4ea2fdd69e9
Michael Tharpe <mtharpe@hashicorp.com>
2021-06-06 16:04:36 -0400

    Remnoved standalone demos which should be coded via Ansible

--- consul-demo/config_files/mysql/mysql.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE admin_database;
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'wordpress';
-GRANT ALL PRIVILEGES ON my_database.* TO 'admin'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
