33d7e99d434908a2e29ede0294d16f0010cf9daf
Mike Tharpe <4297677+mtharpe@users.noreply.github.com>
2021-06-06 16:08:24 -0400

    Mrt provisioner updates (#34)
    * Update to remove shared files from Fedora Workstation
    
    * Update to all make files for Ansible only
    
    * Remnoved standalone demos which should be coded via Ansible

--- consul-demo/tools/rebuild_clients.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for i in web1 web2 db; do vagrant destroy -f $i; done
-for i in web1 web2 db; do vagrant up $i; done
--- vault-demo/scripts/v1/setup-vault.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Setting Vault Systemd to start at boot ..."
-sudo systemctl enable vault.service
-sudo systemctl start vault.service
-sudo systemctl enable consul.service
-sudo systemctl start consul.service
-sleep 15
