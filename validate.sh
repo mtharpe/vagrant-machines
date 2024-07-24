81f9eb50a9cd4d3cf6ee32b79de668fb58e23c62
Michael Tharpe <mtharpe@gmail.com>
2024-07-24 18:09:58 -0400

    Updated script to support provider

--- validate.sh
+++ validate.sh
@@ -2,9 +2,10 @@
 
 export RH_SUBSCRIPTION_MANAGER_USER=""
 export RH_SUBSCRIPTION_MANAGER_PW=""
+export PROVIDER="virtualbox"
 
 for f in `ls -d */`;
 do
-    cd "$f" && echo Validating $f && vagrant validate
+    cd "$f" && echo Validating $f && vagrant validate --provider=$PROVIDER
     cd ..
 done;
