46abcfc8364bebddaa60c405027f005b65dc47a3
Michael Tharpe <mtharpe@users.noreply.github.com>
2026-05-02 22:48:00 -0400

    Split mac and linux platforms with top-level dispatcher

---
+++ linux/validate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export RH_SUBSCRIPTION_MANAGER_USER=""
+export RH_SUBSCRIPTION_MANAGER_PW=""
+export PROVIDER="${PROVIDER:-virtualbox}"
+
+vm_root="${VM_ROOT:-.machines}"
+
+for f in ./template-vm "./$vm_root"/*/; do
+    [ -d "$f" ] && [ -f "$f/Vagrantfile" ] || continue
+    ( cd "$f" && echo "Validating $f" && vagrant validate -p )
+done
